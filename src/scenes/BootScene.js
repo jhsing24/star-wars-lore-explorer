76d2d7f0a15a435e8309b1bb46abdeeebfa63a07
jhsing24 <jerohsing@gmail.com>
2026-07-01 09:31:15 +0800

    feat: register QuestService in the boot registry

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,7 +1,9 @@
 import Phaser from 'phaser'
 import SaveService from '../services/SaveService.js'
 import LoreService from '../services/LoreService.js'
-import { allLore } from '../data/loreData.js'
+import QuestService from '../quests/QuestService.js'
+import { allLore, indexById } from '../data/loreData.js'
+import { getQuests } from '../data/quests.js'
 
 export default class BootScene extends Phaser.Scene {
   constructor() { super('BootScene') }
@@ -11,8 +13,15 @@ export default class BootScene extends Phaser.Scene {
     save.load()
     const lore = new LoreService({ entries: allLore })
 
+    const byId = indexById(allLore)
+    const holoById = new Map(
+      allLore.filter(e => e.category === 'holocrons').map(e => [e.id, e])
+    )
+    const quests = new QuestService({ quests: getQuests(), save, holoById })
+
     this.registry.set('save', save)
     this.registry.set('lore', lore)
+    this.registry.set('quests', quests)
     this.registry.set('uiOpen', false)
 
     this.scene.start('GalaxyMapScene')
